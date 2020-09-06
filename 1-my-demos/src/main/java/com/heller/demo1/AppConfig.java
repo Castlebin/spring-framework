b09642b2e441e7e8505fb70db64d4f8ec30eb3f7
heller <heller@heller.com>
2020-09-06 16:59:39 +0800

    加AOP（为何没有生效？？）

--- 1-my-demos/src/main/java/com/heller/demo1/AppConfig.java
+++ 1-my-demos/src/main/java/com/heller/demo1/AppConfig.java
@@ -1,7 +1,9 @@
 package com.heller.demo1;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy
 @ComponentScan
 public class AppConfig {
 
---
+++ 1-my-demos/src/main/java/com/heller/demo1/aop/XIntercepter.java
@@ -0,0 +1,37 @@
+package com.heller.demo1.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+
+@Aspect
+public class XIntercepter {
+
+	@Pointcut("execution(* com.heller.demo1.service.X.*(..))")
+	public void pointCut() { }
+
+	@Before("pointCut()")
+	public void before() {
+		System.out.println("...before...");
+	}
+
+	@After("pointCut()")
+	public void after() {
+		System.out.println("...after...");
+	}
+
+	@Around("pointCut()")
+	public Object around(ProceedingJoinPoint pjp) {
+		System.out.println("...around before...");
+
+		Object result = null;
+		try {
+			result = pjp.proceed();
+		} catch (Throwable e) {
+			e.printStackTrace();
+		}
+
+		System.out.println("...around after...");
+		return result;
+	}
+
+}
