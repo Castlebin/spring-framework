667ab9435a31ff9623d16de12473e4c32d837a4b
heller <heller@heller.com>
2020-09-06 17:56:02 +0800

    分析AOP增强过程

--- 1-my-demos/src/main/java/com/heller/demo1/aop/WIntercepter.java
+++ 1-my-demos/src/main/java/com/heller/demo1/aop/WIntercepter.java
@@ -6,24 +6,24 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
-public class XIntercepter {
+public class WIntercepter {
 
-	@Pointcut("execution(* com.heller.demo1.service.X.*(..))")
+	@Pointcut("execution(* com.heller.demo1.service.W.*(..))")
 	public void pointCut() { }
 
 	@Before("pointCut()")
 	public void before() {
-		System.out.println("...x before...");
+		System.out.println("...w before...");
 	}
 
 	@After("pointCut()")
 	public void after() {
-		System.out.println("...x after...");
+		System.out.println("...w after...");
 	}
 
 	@Around("pointCut()")
 	public Object around(ProceedingJoinPoint pjp) {
-		System.out.println("...x around before...");
+		System.out.println("...w around before...");
 
 		Object result = null;
 		try {
@@ -32,7 +32,7 @@ public class XIntercepter {
 			e.printStackTrace();
 		}
 
-		System.out.println("...x around after...");
+		System.out.println("...w around after...");
 		return result;
 	}
 
