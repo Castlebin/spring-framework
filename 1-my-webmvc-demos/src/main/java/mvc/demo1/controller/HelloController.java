4cf85858f53e09dd1425ed853f4241cae9281273
yuxingao2 <yuxingao2@creditease.cn>
2020-09-10 15:57:38 +0800

    s

--- 1-my-webmvc-demos/src/main/java/mvc/demo1/controller/HelloController.java
+++ 1-my-webmvc-demos/src/main/java/mvc/demo1/controller/HelloController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
+	@RequestMapping("/sayHello")
 	public String sayHello(String name) {
 		return "Hello, " + name;
 	}
