cae7d4ce106c225ab39af48d2b3f0cc73ef9d8f0
heller <heller@heller.com>
2020-09-13 10:08:15 +0800

    官网原始例子，放入 servlet 容器中才可以执行，实现了 WebApplicationInitializer 接口 ，会被 servlet 容器 启动时，进行初始化，因此可以被执行到
    注意，demo 访问 url 为： http://localhost:8080/demo0/demo0/hello?name=LiLei

--- 1-my-webmvc-demos/src/main/java/mvc/demo0/controller/HelloController.java
+++ 1-my-webmvc-demos/src/main/java/mvc/demo0/controller/HelloController.java
@@ -8,8 +8,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/demo0")
 public class HelloController {
 
-	// !! URL  注册 DispatcherServlet 有个 demo0 的前缀 ，之后，才是代码中 我们自定义的url
+	// !! URL  注册 DispatcherServlet 有个 demo0 的前缀 ，之后，才是代码中 我们自定义的url (默认定义容器contextPath 为 / )
 	// http://localhost:8080/demo0/demo0/hello?name=LiLei
+
+	// 如果定义了 容器的 contextpath 为 xxxx， 那么 访问的url相应的变为 ：
+	// http://localhost:8080/xxxx/demo0/demo0/hello?name=LiLei
 	@RequestMapping("/hello")
 	@ResponseBody
 	public String sayHello(String name) {
