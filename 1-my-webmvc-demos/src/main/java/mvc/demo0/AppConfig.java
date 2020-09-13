a5ee9f4fcbb6f1af7919b9be47272c6c65ee8b6a
heller <heller@heller.com>
2020-09-13 08:04:34 +0800

    官网原始例子，放入 servlet 容器中才可以执行，实现了 WebApplicationInitializer 接口 ，会被 servlet 容器 启动时，进行初始化，因此可以被执行到

---
+++ 1-my-webmvc-demos/src/main/java/mvc/demo0/AppConfig.java
@@ -0,0 +1,10 @@
+package mvc.demo0;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("mvc.demo1")
+public class AppConfig {
+
+}
