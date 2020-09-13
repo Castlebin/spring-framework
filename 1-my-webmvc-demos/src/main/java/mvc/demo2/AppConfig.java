e76f514e360ce3bf327299e1817ad4233c0cf3c7
heller <heller@heller.com>
2020-09-13 11:26:54 +0800

    使用内嵌的tomcat，来注册我们的应用 （类似于 spring-boot ）
    更近一步

---
+++ 1-my-webmvc-demos/src/main/java/mvc/demo2/AppConfig.java
@@ -0,0 +1,12 @@
+package mvc.demo2;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan("mvc.demo2")
+//@EnableWebMvc
+public class AppConfig {
+
+}
---
+++ 1-my-webmvc-demos/src/main/java/mvc/demo2/SpringApplication.java
@@ -0,0 +1,40 @@
+package mvc.demo2;
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Wrapper;
+import org.apache.catalina.startup.Tomcat;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class SpringApplication {
+	public static void run() {
+		Tomcat tomcat = new Tomcat();
+		tomcat.setPort(8080);
+
+		// 注意，这里的 nihao，也要作为 url path 的前缀 ！！！
+	//	tomcat.addWebapp("/nihao", "/Users/heller/tmp");
+		// 使用 了 addContext() 代替 addWebapp() ，所以 不会 需要 jsp 包，不会启动时报 jsp 的异常了
+		// Spring Boot 内部也是如此
+		tomcat.addContext("/nihao", "/Users/heller/tmp");
+
+		AnnotationConfigWebApplicationContext ac = new AnnotationConfigWebApplicationContext();
+		ac.register(AppConfig.class);
+
+		// 如果 配置类上 加了 @EnableWebMvc ，此处 refresh() 会发生异常，启动失败  （理解一下）
+	//	ac.refresh();
+
+		DispatcherServlet servlet = new DispatcherServlet(ac);
+		// 此处的 tomcat.addServlet() 中 contextPath 参数，必须 和 上面 tomcat.addContext() 中 一样 （理解一下）
+		Wrapper demo2 = tomcat.addServlet("/nihao", "demo2", servlet);
+		demo2.setLoadOnStartup(1);
+		demo2.addMapping("/demo2/*");
+
+		try {
+
+			tomcat.start();
+			tomcat.getServer().await();
+		} catch (LifecycleException e) {
+			e.printStackTrace();
+		}
+	}
+}
